a4e3c5a82cb10b84077d7ea66142a578c65a62ae
sayhamza <sayedhamza88882651@gmail.com>
2024-01-08 03:50:27 +0530

    Your commit message

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://SocialApp:SocialApp@socialdb.kmkybsp.mongodb.net/SocialDB?retryWrites=true&w=majority";
+
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    })
+    .catch((error) => console.log("Error: ", error));
+};
